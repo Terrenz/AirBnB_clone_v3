f6f133684e487e4da88501f651e1451f02746dc1
Terrence <terrencepeters54@gmail.com>
2022-10-24 14:31:20 +0100

    new

--- models/__init__.py
+++ models/__init__.py
@@ -1,21 +1,17 @@
-import os
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
-"""CNC - dictionary = { Class Name (string) : Class Type }"""
+#!/usr/bin/python3
+"""
+initialize the models package
+"""
 
-if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
-    from models.engine import db_storage
-    CNC = db_storage.DBStorage.CNC
-    storage = db_storage.DBStorage()
+from os import getenv
+
+# DBSTORAGE
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+# FILESTORAGE
 else:
-    from models.engine import file_storage
-    CNC = file_storage.FileStorage.CNC
-    storage = file_storage.FileStorage()
-
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
